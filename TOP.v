d6fc166ae4a11cb5bb8f2accb5d5693df42eeb9f
Ignacio.Aichino <ignacio.aichino@urbetrack.com>
2024-07-29 23:03:38 -0300

    first commit with tx data debug

--- TOP.v
+++ TOP.v
@@ -19,16 +19,20 @@ module TOP
         output  wire        [3:0]   o_debug_state  //conozco el estado en el que esta el debug
     );
 
-        localparam  MEM_INSTR_SIZE = $clog2(SIZE_MEM_INSTRUC);
+        localparam  MEM_INSTR_SIZE      = $clog2(SIZE_MEM_INSTRUC);
+        localparam  MEM_REGISTERS_SIZE  = $clog2(BITS_SIZE);
 
 
         wire    [BITS_SIZE-1:0]         select_mem_dir;
-        wire    [MEM_INSTR_SIZE-1:0]    select_mem_ins_dir;//Sirve para contar cada posicion de la memoria de instrucciones. Es decir cada posicion donde se va alojar una instr
+        wire    [MEM_INSTR_SIZE-1:0]    select_mem_ins_dir;  //Sirve para contar cada posicion de la memoria de instrucciones. Es decir cada posicion donde se va alojar una instr
+        wire    [MEM_REGISTERS_SIZE-1:0]     select_reg_dir; //se incrementa en 1 para que mips manda a la salida el valor de ese reg_file
+        
         wire                            i_ctl_clk_wiz;  //segun el modo o si esta en stop,(1,0) con 1 se incrementa el clock, es control
         wire    [BITS_SIZE-1:0]         dato_mem_ins;
         wire                            flag_write_mem_instr;   //cuando recibi el dato(rx) se pone a 1 para escribir mem instruc
         wire    [BITS_SIZE-1:0]         pc;
         wire    [BITS_SIZE-1:0]         data_mem;
+        wire                            halt;
 
 
         wire                            uart_rx_reset;
@@ -64,6 +68,7 @@ module TOP
         .i_reset                        (i_reset),
         .i_ctl_clk_wiz                  (i_ctl_clk_wiz),
         .i_select_address_mem_instr     (select_mem_ins_dir),
+        .i_select_address_register      (select_register_dir),
         .i_dato_mem_ins                 (dato_mem_ins),
         .i_flag_write_mem_ins           (flag_write_mem_instr),
         .o_pc                           (pc)
@@ -96,6 +101,7 @@ module TOP
     (
         .i_clk                  (wire_clk_wz),
         .i_reset                (i_reset),
+        .i_halt                 (halt),
         .i_uart_rx_flag_ready   (uart_rx_done),
         .i_uart_rx_data         (uart_rx_data),
         .i_uart_tx_done         (uart_tx_done),
@@ -103,6 +109,7 @@ module TOP
         .o_uart_rx_reset        (uart_rx_reset),
         .o_ctl_clk_wiz          (o_ctl_clk_wiz),
         .o_select_mem_ins_dir   (select_mem_ins_dir),
+        .o_select_register_dir  (select_register_dir),
         .o_dato_mem_ins         (dato_mem_ins),
         .o_flag_instr_write     (flag_write_mem_instr),
         .o_debug_state          (o_debug_state)
